815758b438fa98dda0803e3c14e128a814072761
Task #1. Autocode Greetings <magzhansaken@mail.ru>
2022-12-15 17:02:18 +0600

    initial commit

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-
-        System.out.println( "java\n" +"is\n" + "nsmth");
+        System.out.println( "java\n" +"is\n" + "smth");
     }
 }
